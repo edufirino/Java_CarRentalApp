5d66b2a9be0500ca88bba6662b266c3036f3935d
gubaby <68657994+gubaby@users.noreply.github.com>
2020-07-22 14:55:56 -0300

    Update Test.java
    Uma delicia de mudança

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 class Test {
     public static void main(String[] args) {
-        System.out.println("Mudanças deliciosas de testar");
+        System.out.println("Mudanças deliciosas e funcionais");
     }
 }
