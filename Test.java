4dd70efd7ef733309ab92a9a06d20eb39f1aef67
gubaby <68657994+gubaby@users.noreply.github.com>
2020-07-22 14:55:05 -0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 class Test {
     public static void main(String[] args) {
-        System.out.println("Testes Deliciosamente deliciosos");
+        System.out.println("Mudanças deliciosas de testar");
     }
 }
