de906d6e1a4f4678c2ff5368d09beb0a890d1454
dudulofu <edufirino@gmail.com>
2020-07-23 20:36:11 -0300

    randomItem created

--- src/model/Car.java
+++ src/model/Car.java
@@ -1,37 +1,37 @@
-public class Car 
-{
+public class Car {
 
-    
     public String getRegNumber() {
         return regNumber;
     }
+
     public void setRegNumber(String regNumber) {
         this.regNumber = regNumber;
     }
 
-   
     public String getMake() {
         return make;
     }
+
     public void setMake(String make) {
         this.make = make;
     }
 
-    
     public String getModel() {
         return model;
     }
+
     public void setModel(String model) {
         this.model = model;
     }
 
-    
     public String getNumberSeats() {
         return numberSeats;
     }
+
     public void setNumberSeats(String numberSeats) {
         this.numberSeats = numberSeats;
     }
+
     private String regNumber;
     private String make;
     private String model;
